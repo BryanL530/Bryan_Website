8de9c87ac98c8880f6500439b443594447254eb9
BryanL530 <bryanlin530@gmail.com>
2023-06-19 22:59:17 -0700

    End of day 3 files

--- Script/script_home.js
+++ Script/script_home.js
@@ -1,7 +1,12 @@
 const html = document.querySelector('html');
+const content = document.querySelector('.content');
+let count = 1;
 
 html.onclick = doSomething;
 
+
 function doSomething() {
-    console.log("do something");
+    const li = document.createElement('ul');
+    li.textContent = 'test ' + count++;
+    content.appendChild(li);
 }
