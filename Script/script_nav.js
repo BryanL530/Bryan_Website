cc76ff7369c5116182931d83dbb94d992ef40010
BryanL530 <bryanlin530@gmail.com>
2023-07-13 16:05:19 -0700

    More test changes to fix href

--- Script/script_nav.js
+++ Script/script_nav.js
@@ -10,25 +10,25 @@ links.className = "link-list";
 const home = document.createElement("a");
 home.className = "nav-link";
 home.textContent = "HOME";
-home.href = "/index.html";
+home.href = "index.html";
 links.appendChild(home);
 
 const aboutMe = document.createElement("a");
 aboutMe.className = "nav-link";
 aboutMe.textContent = "ABOUT ME";
-aboutMe.href = "/about_me.html";
+aboutMe.href = "about_me.html";
 links.appendChild(aboutMe);
 
 const project = document.createElement("a");
 project.className = "nav-link";
 project.textContent = "PROJECT";
-project.href = "/project.html";
+project.href = "project.html";
 links.appendChild(project);
 
 const devLog = document.createElement("a");
 devLog.className = "nav-link";
 devLog.textContent = "DEVELOPEMENT LOG";
-devLog.href = "/dev_log.html";
+devLog.href = "dev_log.html";
 links.appendChild(devLog);
 
 document.body.prepend(nav);
